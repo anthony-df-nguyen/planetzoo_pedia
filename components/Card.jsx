b58e2ea35d54a31b5f73def27c1415b56c03a235
skifreetony <skifreetony@yahoo.com>
2021-03-28 12:31:37 -0700

    begin image downloads

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image"
 
 export default function Card(props) {
-    const fullCard = (e) => {
-      const name = e.target.textContent;
-      props.click(name);
-    }
-    return (
-      <div className="card" onClick={(e) => fullCard(e)}>
-        <div
-          className="animalPicture"
-          style={{ backgroundImage: `url(${props.pic})` }}></div>
-        <div className="animalName">{props.name}</div>
+  const fullCard = (e) => {
+    const name = e.target.textContent;
+    props.click(name);
+  };
+
+
+  return (
+    <div className="card" onClick={(e) => fullCard(e)}>
+      <div className="animalPicture" >
+        <Image src={props.pic} height="64" width="64" layout="fixed" quality={1}/>
       </div>
-    );
+      <div className="animalName">{props.name}</div>
+    </div>
+  );
 }
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: ["cms-cdn.zaonce.net", "d1zb04s4od4oai.cloudfront.net"],
+  },
+};
